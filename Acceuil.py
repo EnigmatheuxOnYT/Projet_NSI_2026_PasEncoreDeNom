3d3f215a6a498b127ac4880623b4a84f7e110d90
Xernori <Xernori.alt@gmail.com>
2025-10-02 19:39:09 +0200

    Test alt et proposition class Accueil

--- Acceuil.py
+++ Acceuil.py
@@ -29,9 +29,48 @@ def test():
     print('test')
     return None
 
+
+class Accueil:
+    def __init__(self,ecran):
+        self.ecran = ecran
+        self.horloge = pygame.time.Clock()
+        self.fps = 60
+        self.running = False
+
+    def gerer_entree(self): #Gère les entrées du joueur
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            elif event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+    
+    
+    def dessiner_fenetre (self):
+        screen.fill((155,200,175))
+        pygame.display.flip()
+    
+    def lancer(self):
+        self.running = True
+
+        while self.running:
+            self.gerer_entree()
+            self.dessiner_fenetre()
+            self.horloge.tick(self.fps)
+        
+
+    
+
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption("Pas de Nom")
+    pygame.display.set_caption("Jeu")
+    #pygame.display.set_icon("icon.ico")
     screen = pygame.display.set_mode((1280,720),pygame.RESIZABLE)
-    startGame()
+    #startGame()
+    jeu = Accueil(screen)
+    jeu.lancer()
     pygame.quit()
